c89cfd097848d84e0de830386bfdcd0d4929d7d0
aretepsn <aretepsn@gmail.com>
2025-12-01 00:08:59 +0900

    Update plan_goods_run.sh

--- plan_goods_run.sh
+++ plan_goods_run.sh
@@ -5,6 +5,26 @@ set -e  # <--- 이 명령어를 추가하면 에러 발생 시 즉시 종료됨
 
 
 echo "작업시작"
-python ./plan_goods/plan_goods.py 202511305554580 10
-python ./plan_goods/plan_goods.py 202511305554590 10
+python ./plan_goods/plan_goods.py 202511305554569 1000
+python ./plan_goods/plan_goods.py 202511305555569 1000
+python ./plan_goods/plan_goods.py 202511305556569 1000
+python ./plan_goods/plan_goods.py 202511305557569 1000
+python ./plan_goods/plan_goods.py 202511305558569 1000
+python ./plan_goods/plan_goods.py 202511305559569 1000
+python ./plan_goods/plan_goods.py 202511305560569 1000
+python ./plan_goods/plan_goods.py 202511305561569 1000
+python ./plan_goods/plan_goods.py 202511305562569 1000
+python ./plan_goods/plan_goods.py 202511305563569 1000
+
+python ./plan_goods/plan_goods.py 202511305564569 1000
+python ./plan_goods/plan_goods.py 202511305565569 1000
+python ./plan_goods/plan_goods.py 202511305566569 1000
+python ./plan_goods/plan_goods.py 202511305567569 1000
+python ./plan_goods/plan_goods.py 202511305568569 1000
+python ./plan_goods/plan_goods.py 202511305569569 1000
+python ./plan_goods/plan_goods.py 202511305570569 1000
+python ./plan_goods/plan_goods.py 202511305571569 1000
+python ./plan_goods/plan_goods.py 202511305572569 1000
+python ./plan_goods/plan_goods.py 202511305573569 1000
+
 echo "작업끝"

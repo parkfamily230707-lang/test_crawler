9a5628f855a145f1afdfa09b51086d2c0ca51c5b
aretepsn <aretepsn@gmail.com>
2025-11-30 23:48:31 +0900

    .

--- plan_goods_run.sh
+++ plan_goods_run.sh
@@ -1,5 +1,9 @@
 # run.sh 파일 내용 아래와 같이 프로그램을 백그라운드에서 수행하도록 명령어 지정
 # >nohup ./run.sh &
+
+set -e  # <--- 이 명령어를 추가하면 에러 발생 시 즉시 종료됨
+
+
 echo "작업시작"
 python ./plan_goods/plan_goods.py 202511305554580 10
 python ./plan_goods/plan_goods.py 202511305554590 10
